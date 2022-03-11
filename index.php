64da08648fd0432695b704c9e6e4208f0a777813
0xAgentG <101059128+0xAgentG@users.noreply.github.com>
2022-03-10 20:36:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<style>
+		body {
+			background:url('code.jpg');
+		}
+		</style>
+	</head>
+	<body>
+		<BR><BR><BR><BR>
+		<center>
+		<img src='agentlogo.jpg' />
+		<BR><BR>
+		<img src='agent1.jpg' />
+		<BR><BR><BR>
+		<form>
+			<input type='text' name='code' size=15' /><input type='submit' value='Enter Code' />
+		</form>
+		</center>
+	</body>
+</html>
